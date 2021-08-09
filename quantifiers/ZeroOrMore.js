32639799af567b4a53acdbf745eddf9c9c66e8e8
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-08-09 19:07:26 -0300

    Zero or more

---
+++ quantifiers/ZeroOrMore.js
@@ -0,0 +1,8 @@
+const texto = 'De longe avistei o fogo e uma pessoa gritando: FOGOOO!';
+const texto2 = 'There is big fog in NYC';
+
+// * 0 | +
+const regex = /fogo*/gi;
+
+console.log(texto.match(regex));
+console.log(texto2.match(regex));
