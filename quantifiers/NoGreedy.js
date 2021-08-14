2a84341c138c162216384abd8162ae274b7b8786
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-08-14 16:26:47 -0300

    greedy and no greedy

---
+++ quantifiers/NoGreedy.js
@@ -0,0 +1,9 @@
+const texto = "<div>Conteudo 01</div><div>Conteudo 02</div>";
+
+console.log(texto.match(/<div>.+<\/div>/g));
+console.log(texto.match(/<div>.*<\/div>/g));
+console.log(texto.match(/<div>.{0,100}<\/div>/g));
+
+console.log(texto.match(/<div>.+?<\/div>/g));
+console.log(texto.match(/<div>.*?<\/div>/g));
+console.log(texto.match(/<div>.{0,100}?<\/div>/g));
